c2f292768d30a797b73bceaab1ef4252e536a778
zuhairnashif <zuhairnashif86@gmail.com>
2025-11-06 01:03:09 +0700

    feat: buttom tab bar

---
+++ app/(tabs)/pengaturan.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const pengaturan = () => {
+  return (
+    <View>
+      <Text>pengaturan</Text>
+    </View>
+  )
+}
+
+export default pengaturan
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/teman.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const teman = () => {
+  return (
+    <View>
+      <Text>teman</Text>
+    </View>
+  )
+}
+
+export default teman
+
+const styles = StyleSheet.create({})
